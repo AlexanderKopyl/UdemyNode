c907440a59e7137417d369a9f1865c022373b497
Alexsander <Kopul@ua.fm>
2019-06-28 18:34:38 +0300

    Add playground and some information about object work es6

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,23 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Dnepr Ukrain', (error,data)=>{
-    if(error){
-        console.log(error)
-    }else {
-        forecast(data.longtitude,data.latitude,(error,data)=>{
-            if(error){
-                console.log(error);
-            }else {
-                console.log(data);
-            }
-        });
-    }
-});
+if(!process.argv[2]){
+    console.log("Provide please sity");
+}else{
+    geocode(process.argv[2], (error,data)=>{
+        if(error){
+            console.log(error)
+        }else {
+            forecast(data.longtitude,data.latitude,(error,forecastdata)=>{
+                if(error){
+                    console.log(error);
+                }else {
+                    console.log(data.place_name);
+                    console.log(forecastdata);
+                }
+            });
+        }
+    });
+
+}
 
