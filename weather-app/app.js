8636eb0d9bd952f27d474248c76a76a1df5d0714
Alexsander <Kopul@ua.fm>
2019-06-28 15:48:20 +0300

    Add function geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,52 +1,50 @@
 const request = require('request');
-
+const geocode = require('./utils/geocode');
 const url = {
     urls: 'https://api.darksky.net/forecast/',
     apiKey: 'f7e71606d23737657a8bf5d2b7937428',
-    // out: this.urls + this.apiKey + '/' + this.long + "," + this.lat
-    out() {
-        return this.urls + this.apiKey + '/' + this.long + "," + this.lat
-    }
-};
-const mapBoxUrl = {
-    url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
-    apiKey: 'pk.eyJ1IjoiYWxleHNhbmRlcjExMTE1IiwiYSI6ImNqeGRheDU4bTBiZjMzb3M4bmJmdTZvd2YifQ.LcuiK8noc8bF1kayZJD1kw',
-    sity: 'Dnepr.json',
-    limit: 3,
+    lang: 'ru',
+    units: 'auto',
 
     out() {
-        return this.url + this.sity + "?limit=" + this.limit + "&access_token=" + this.apiKey
+        return this.urls + this.apiKey + '/' + this.long + "," + this.lat+"?lang=" + this.lang + "&units=" + this.units
     }
 };
 
+// request({url:url.out(), json: true},(error, response, body)=>{
+//     if(error){
+//         console.log('Error connection');
+//     }else if( body.error){
+//         console.log('Error: Not enough information');
+//     }else{
+//         const temperature = body.currently.apparentTemperature;
+//         const rainChance = body.currently.precipProbability;
+//         const summary = body.daily.data[0].summary;
+//         console.log(`${summary} It is currently ${temperature} degrees out. There is a ${rainChance}% chance of rain.`);
+//
+//     }
+// });
 
-request({url:mapBoxUrl.out(), json: true},(error, response, body)=>{
+
+geocode('Dnepr Ukrain', (error,data)=>{
     if(error){
-        console.log("Error connection");
-    }else{
-        const dnipro = body.features.filter((item) => item.id === 'poi.2808908687579');
-        url.long = dnipro[0].center[1];
-        url.lat = dnipro[0].center[0];
+        console.log(error)
+    }else {
+        url.lat = data.latitude;
+        url.long = data.longtitude;
+        request({url: url.out(), json: true}, (error, response, body) => {
+            if (error) {
+                console.log('Error connection');
+            } else if (body.error) {
+                console.log('Error: Not enough information');
+            } else {
+                const temperature = body.currently.apparentTemperature;
+                const rainChance = body.currently.precipProbability;
+                const summary = body.daily.data[0].summary;
+                console.log(data.place_name,`${summary} It is currently ${temperature} degrees out. There is a ${rainChance}% chance of rain.`);
+
+            }
+        });
     }
-
-
-    request({url:url.out(), json: true},(error, response, body)=>{
-        if(error){
-            console.log('Error connection');
-        }else if( body.error){
-            console.log('Error: Not enough information');
-        }else{
-            const temperature = body.currently.apparentTemperature;
-            const rainChance = body.currently.precipProbability;
-            const summary = body.daily.data[0].summary;
-            console.log(`${summary} It is currently ${temperature} degrees out. There is a ${rainChance}% chance of rain.`);
-
-        }
-
-
-
-    });
 });
-
-
 
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const geocode = (address,callback) =>{
+
+    const mapBoxUrl = {
+        url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
+        apiKey: 'pk.eyJ1IjoiYWxleHNhbmRlcjExMTE1IiwiYSI6ImNqeGRheDU4bTBiZjMzb3M4bmJmdTZvd2YifQ.LcuiK8noc8bF1kayZJD1kw',
+        sity: encodeURIComponent(address) + '.json',
+        limit: 3,
+
+        out() {
+            return this.url + this.sity + "?limit=" + this.limit + "&access_token=" + this.apiKey
+        }
+    };
+
+    request({url:mapBoxUrl.out(), json: true},(error, response, body)=>{
+        if(error){
+            callback("Error connection",undefined);
+        }else if(body.features.length === 0){
+            callback('Undefined place try another serch',undefined);
+        }else{
+            // const dnipro = body.features.filter((item) => item.id === 'poi.2808908687579');
+            callback(undefined,{
+                latitude: body.features[0].center[0],
+                longtitude:  body.features[0].center[1],
+                place_name:body.features[0].place_name
+            });
+        }
+    });
+};
+
+module.exports = geocode;
